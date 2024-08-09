04a20bb3e4f888d1e96edfe3c556fa5138be3b6d
Dhnanajay Kumar <dk8818604@gmail.com>
2024-08-09 22:47:33 +0530

    final

---
+++ Codeforces/GreaterThen1300LessThen1399/76.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    int m;
+    cin >> m;
+    vector<vector<int>> v(m, vector<int>(2));
+    vector<int> count(n, 0);
+    
+    // Precompute the count of adjacent equal characters for each prefix
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            count[i + 1] = count[i] + 1;
+        } else {
+            count[i + 1] = count[i];
+        }
+    }
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> v[i][j];
+            v[i][j]--;
+        }
+        // Calculate the count of adjacent equal characters for the query
+        int x = count[v[i][1]] - count[v[i][0]];
+        cout << x << endl;
+    }
+    
+    return 0;
+}
